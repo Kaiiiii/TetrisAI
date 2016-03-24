96bf88db1fda4ebc86a38d334590ffb41546af5f
Mai Anh Vu <me@maianhvu.com>
2016-03-25 00:16:35 +0800

    modify universal heuristics to give better results

--- src/main/java/ai/UniversalPlayer.java
+++ src/main/java/ai/UniversalPlayer.java
@@ -10,10 +10,10 @@ public class UniversalPlayer extends HeuristicsPlayer {
 
     @Override
     protected int calculateHeuristics(Analysis analysis) {
-        return -7 * analysis.getHeight() +
+        return -1 * analysis.getHeight() +
                 -2 * analysis.getRoughness() +
-                -3 * analysis.getHolesCount() +
-                -5 * analysis.getCellsCount() +
-                -1 * analysis.getHighestSlope();
+                -10 * analysis.getHolesCount() +
+                -7 * analysis.getCellsCount() +
+                -5 * analysis.getHighestSlope();
     }
 }
